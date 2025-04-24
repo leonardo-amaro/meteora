06089d3029a4d83a1ff28153438730d36da397d7
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-04-24 09:11:48 -0300

    removendo prop drilling #2

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -3,21 +3,17 @@ import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
 
-const Produtos = ({ adicionarProduto }) => {
+const Produtos = () => {
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
       <div className="container row mx-auto">
         {produtos.map((produto) => (
-          <Produto
-            key={produto.id}
-            {...produto}
-            adicionarProduto={adicionarProduto}
-          />
+          <Produto key={produto.id} {...produto} />
         ))}
       </div>
     </section>
   );
-};
+}
 
 export default Produtos;
